4223d0dbb48e50b73552c5e757d7068fc66355d1
Alessandro <ale.diliberti@gmail.com>
2022-11-17 23:26:14 +0100

    First tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, SafeAreaView, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Constants from 'expo-constants'
 
-export default function App() {
+function FeedScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', paddingTop: '20%' }}>
+
+      <Text>Feed</Text>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function NotificationsScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </SafeAreaView>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={{
+          tabBarActiveTintColor: '#e91e63',
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarStyle: { backgroundColor: 'powderblue',paddingTop: Constants.statusBarHeight },
+        }}
+      >
+
+        <Tab.Screen
+          name="Feed"
+          component={FeedScreen}
+          options={{ tabBarLabel: 'Home' }}
+
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={NotificationsScreen}
+          options={{ tabBarLabel: 'Updates' }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ tabBarLabel: 'Profile' }}
+        />
+      </Tab.Navigator>
+    </>
+  );
+}
+export default function App() {
+  return (
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+
+
+  );
+}
